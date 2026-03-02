96ac49186b15be70ca76bf10599117593070aeb2
Blackpa77 <h1101241029@student.untan.ac.id>
2026-03-02 22:15:22 +0700

    praktikum 5

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { Elysia, t } from "elysia";
 
 const app = new Elysia()
 
-  //PRAKTIKUM 4
+//PRAKTIKUM 4
   .onRequest(({ request, set }) => {
     console.log(`[${new Date().toISOString()}] ${request.method} ${request.url}`);
     if (request.headers.get("x-test") === "block") {
@@ -11,3 +11,22 @@ const app = new Elysia()
     }
   })
   .get("/hello", () => "Hello Middleware")
+
+//PRAKTIKUM 5
+  .get(
+    "/admin",
+    () => {
+      return { stats: 99 };
+    },
+    {
+      beforeHandle({ headers, set }) {
+        if (headers.authorization !== "Bearer 123") {
+          set.status = 401;
+          return {
+            success: false,
+            message: "Unauthorized",
+          };
+        }
+      },
+    }
+  )
